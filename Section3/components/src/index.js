68a3ffe3704f31119465ef9d9256549ff194b516
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-14 00:37:11 +0200

    41. Showing custom children

--- Section3/components/src/index.js
+++ Section3/components/src/index.js
@@ -2,17 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return ( 
-        <div className="ui container comments">
-            <CommentDetail author="Sam" avatar={faker.image.avatar()} timeAgo="Today at 4:45PM" commentText="Great blog dude!"/>
-            <CommentDetail author="Alex" avatar={faker.image.avatar()} timeAgo="Today at 2:00AM" commentText="Nice one!"/>
-            <CommentDetail author="Jane" avatar={faker.image.avatar()} timeAgo="Yesterday at 5:00PM" commentText="Very pretty!"/>
-        </div>
-
-
-    );
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          avatar={faker.image.avatar()}
+          timeAgo="Today at 4:45PM"
+          content="Great blog dude!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          avatar={faker.image.avatar()}
+          timeAgo="Today at 2:00AM"
+          content="Nice one!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          avatar={faker.image.avatar()}
+          timeAgo="Yesterday at 5:00PM"
+          content="Very pretty!"
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
