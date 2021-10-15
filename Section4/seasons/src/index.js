cbdc306aee4ed4a3c79d5bf3e7ecebdb83868760
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-15 22:59:18 +0200

    60. Conditionally rendering content

--- Section4/seasons/src/index.js
+++ Section4/seasons/src/index.js
@@ -32,13 +32,11 @@ class App extends React.Component {
 
   // defining a class render method is obligatory in React!
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat} </div>;
+    } else return <div>Loading!</div>;
   }
 }
 
