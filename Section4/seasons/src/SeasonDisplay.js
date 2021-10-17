e074caf719e2ade12067dd5af2656cbde5a3a729
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-17 21:52:25 +0200

    Some refactoring

--- Section4/seasons/src/SeasonDisplay.js
+++ Section4/seasons/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    message: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    message: "Burr it's cold!",
+    iconName: "snowflake",
+  },
+};
+
 const getSeason = (lat, month) => {
   if ((month > 2) & (month < 9)) {
     return lat > 0 ? "summer" : "winter";
@@ -8,15 +19,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const message =
-    season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!";
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const { message, iconName } = seasonConfig[season];
 
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{message}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
