4611f401c606e127ad70c74ff015baa639d40aa4
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-20 17:14:36 +0200

    97. Handling requests with Async Await
    ASYNC onSearchSubmit(term) {
        const response = AWAIT axios.get(...)
    
        console.log(response.data.results);
    }

--- Section7-8-9/pics/src/components/App.js
+++ Section7-8-9/pics/src/components/App.js
@@ -4,19 +4,30 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   // getting response from promise with ".then()"
+  // onSearchSubmit(term) {
+  //   axios
+  //     .get("https://api.unsplash.com/search/photos", {
+  //       params: { query: term },
+  //       headers: {
+  //         Authorization:
+  //           "Client-ID k_H_587UN1PvBw4Y8F354q4bRUcv_7Nn2EudvDKWyr0",
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log(response.data.results);
+  //     });
+  // }
 
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID k_H_587UN1PvBw4Y8F354q4bRUcv_7Nn2EudvDKWyr0",
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-      });
+  // alternate method: Async Await:
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID k_H_587UN1PvBw4Y8F354q4bRUcv_7Nn2EudvDKWyr0",
+      },
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
