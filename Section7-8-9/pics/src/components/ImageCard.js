4e8e4e925f6c3fa7fd57115e9f5d6b5d3bb1396d
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-21 16:48:48 +0200

    111-113. Accessing the DOM with ref & callback on image load

--- Section7-8-9/pics/src/components/ImageCard.js
+++ Section7-8-9/pics/src/components/ImageCard.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.ImageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.ImageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.ImageRef.current.clientHeight);
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.ImageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
