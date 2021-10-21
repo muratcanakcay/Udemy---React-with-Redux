bc5df874c8e591cfbeb94989f0b6987bf981b356
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-21 02:06:05 +0200

    108. Grid CSS

--- Section7-8-9/pics/src/components/ImageList.js
+++ Section7-8-9/pics/src/components/ImageList.js
@@ -1,17 +1,12 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = (props) => {
-  // refactoring (images) --> ({id, description, urls})
   const images = props.images.map(({ id, description, urls }) => {
     return <img key={id} alt={description} src={urls.regular} />;
   });
-  //   const images = props.images.map((image) => {
-  //     return (
-  //       <img key={image.id} alt={image.description} src={image.urls.regular} />
-  //     );
-  //   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
