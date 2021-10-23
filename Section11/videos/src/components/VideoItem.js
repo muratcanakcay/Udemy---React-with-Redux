d7ed637edfccef3a0683f6b1f80c7e42e74cd408
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-23 17:35:34 +0200

    128. Rendering video thumbnails

--- Section11/videos/src/components/VideoItem.js
+++ Section11/videos/src/components/VideoItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const VideoItem = (props) => {
-  return <div>VideoItem</div>;
+const VideoItem = ({ video }) => {
+  return (
+    <div>
+      <img src={video.snippet.thumbnails.medium.url} />
+      <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`}>
+        {video.snippet.title}
+      </a>
+    </div>
+  );
 };
 
 export default VideoItem;
