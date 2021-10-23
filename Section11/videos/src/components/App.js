ad9b5cd3bef3460fbfac92ba386f3733b5651b07
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-23 18:13:30 +0200

    131. Deeply nested callbacks

--- Section11/videos/src/components/App.js
+++ Section11/videos/src/components/App.js
@@ -4,7 +4,7 @@ import VideoList from "./VideoList";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -17,11 +17,18 @@ class App extends React.Component {
     console.log(this.state.videos);
   };
 
+  onVideoSelect = (video) => {
+    console.log("From the App!", video);
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
