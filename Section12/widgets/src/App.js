cfbd306fd19b6d98fd76ed4c623f42bc0ee4faa0
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-24 17:58:25 +0200

    140. Sending items prop to Accordion component

--- Section12/widgets/src/App.js
+++ Section12/widgets/src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "Reach is a front end Javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a popular Javascript library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
