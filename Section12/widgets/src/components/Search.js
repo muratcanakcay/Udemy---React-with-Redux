6eb2943130d9fb56a444d6376963977c69617662
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-25 21:43:37 +0200

    158. Executing async search on wikipedia with axios and useEffect()

--- Section12/widgets/src/components/Search.js
+++ Section12/widgets/src/components/Search.js
@@ -1,7 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) search();
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
