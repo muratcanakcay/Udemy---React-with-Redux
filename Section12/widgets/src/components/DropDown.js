576e0d49c78f91aca08c6324ffcf38c4f88c50f4
Muratcan Akcay <muratcanakcay@gmail.com>
2021-10-30 23:50:30 +0200

    181. Capturing clicks on document body

--- Section12/widgets/src/components/DropDown.js
+++ Section12/widgets/src/components/DropDown.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const DropDown = ({ selected, onSelectedChange, options }) => {
     const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        document.body.addEventListener(
+            "click",
+            () => {
+                setOpen(false);
+            },
+            { capture: true }
+        );
+    }, []);
 
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
