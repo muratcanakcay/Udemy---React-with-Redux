87e28463667e06d966b73fc0aad9f9ae874ba187
Muratcan Akcay <muratcanakcay@gmail.com>
2021-11-07 08:36:43 +0100

    239. Wiring up the provider

--- Section17/songs/src/reducers/index.js
+++ Section17/songs/src/reducers/index.js
@@ -1,4 +1,4 @@
-// Reducers
+import { combineReducers } from "redux"
 
 const songsReducer = () => {
     return [
@@ -16,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
     return selectedSong
 }
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
