6e8f0ea05545058184b16411287c0764ce2051c3
Muratcan Akcay <muratcanakcay@gmail.com>
2021-11-07 08:17:38 +0100

    238. Building reducers

---
+++ Section17/songs/src/reducers/index.js
@@ -0,0 +1,18 @@
+// Reducers
+
+const songsReducer = () => {
+    return [
+        { title: "A Change of Seasons", duration: "25:04" },
+        { title: "Lines in the Sand", duration: "12:21" },
+        { title: "Hollow Years", duration: "7:12" },
+        { title: "Learning to Live", duration: "13:51" }
+    ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === "SONG_SELECTED") {
+        return action.payload
+    }
+
+    return selectedSong
+}
