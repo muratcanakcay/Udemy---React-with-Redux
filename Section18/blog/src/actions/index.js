eb52d22a1d4472ac4949078db2d40d38b88c4619
Muratcan Akcay <muratcanakcay@gmail.com>
2021-11-14 06:18:30 +0100

    260. Making use of Redux Thunk for async action creator

--- Section18/blog/src/actions/index.js
+++ Section18/blog/src/actions/index.js
@@ -1,10 +1,34 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder"
 
-export const fetchPosts = async () => {
+// WRONG WAY TO DO IT:
+
+// export const fetchPosts = async () => {
+//     const response = await jsonPlaceholder.get("/posts")
+
+//     return {
+//         type: "FETCH_POSTS",
+//         payload: response
+//     }
+// }
+
+
+// CORRECT WAY WITH THUNK:
+
+// export const fetchPosts = () => {
+//     return async (dispatch, getState) => {
+//         const response = await jsonPlaceholder.get("/posts")
+
+//         dispatch({
+//             type: "FETCH_POSTS",
+//             payload: response
+//         })
+//     }
+// }
+
+// REFACTORED TO:
+
+export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get("/posts")
 
-    return {
-        type: "FETCH_POSTS",
-        payload: response
-    }
+    dispatch({ type: "FETCH_POSTS", payload: response })
 }
